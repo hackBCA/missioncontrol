e6821da1b8bbb4fdedcfff9cbcb783ebdf231dc0
Robert Kim <rkty13@gmail.com>
2016-02-01 10:46:22 -0500

    Removed unecessary import.

--- application/mod_hacker/views.py
+++ application/mod_hacker/views.py
@@ -2,7 +2,6 @@ from flask import render_template, redirect, request, flash, session
 from flask.ext.login import login_required, current_user
 from . import hacker_module as mod_hacker
 from . import controllers as controller
-from .forms import *
 from application import CONFIG
 
 @mod_hacker.route("/email")
