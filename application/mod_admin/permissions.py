61cbe9ae9e8ca4c0a1c1e55c61803028230c9637
Robert Kim <rkty13@gmail.com>
2016-03-07 19:37:45 -0500

    Fixing minor issues.

--- application/mod_admin/permissions.py
+++ application/mod_admin/permissions.py
@@ -1,17 +1,17 @@
 from flask.ext.principal import Principal, Permission, RoleNeed
 
 roles = [
-"admin", 
-"board", 
-"review_apps",
-"read_data",
-"organizer", 
-"volunteer"
+    "admin", 
+    "board", 
+    "review_apps",
+    "read_data",
+    "organizer", 
+    "volunteer"
 ]
 
 class permissions:
   def __init__(self, roles):    
     for role in roles:
-      setattr(self, role, Permission(RoleNeed(role)))
+        setattr(self, role, Permission(RoleNeed(role)))
 
 sentinel = permissions(roles)
